cbb0ee8d6b6a1cc83b0804c8080fc60376f9f065
Wesley Pereira <wesley.wz@gmail.com>
2019-03-25 12:26:42 -0300

    Acrescimento de Dados

--- TestesVetorizacao/extract-label.py
+++ TestesVetorizacao/extract-label.py
@@ -11,9 +11,12 @@ path = os.getcwd()
 
 
 
+
 # parse an xml file by name
 
 file = path + '/TesteMarcos_anotado.xml' 
+
+file = '/media/wesleyz/HD1Tera/DADOS/000-git/004-DATASETS/000 dataset portarias ifes/dataset-portarias-ifes/NER-50-portarias/ArqsTeste/NER-50-portarias.xml'
 
 
 import xml.etree.ElementTree as ET  
@@ -41,55 +44,60 @@ for elem in root:
         labels.append((doc, feat, tipo))
         
 
-        
-import csv
+
+topo = "<html><head><link rel='stylesheet' type='text/css' href='http://rii.lcad.inf.ufes.br/wpsilvaExperimentos/repo-meia/estilo.css'><meta charset='UTF-8'><link rel='icon' type='image/png' href='./logo-lcad.png' /><title>Diário de Doutoramento - PPGI/UFes</title></head><style>    table {    border-spacing: 0;    width: 100%;    border: 0px solid #ddd;    }    th {    cursor: pointer;    }    th, td {    text-align: left;    padding: 16px;    }    tr:nth-child(even) {       background-color: #f2f2f2    }     h2 {  color: darkblue;   font-family: verdana;   font-size: 100%;  }   p{   color: black;  font-family: courier;   font-size: 100%; }     </style> <body><h1 align='center'> <img src='http://rii.lcad.inf.ufes.br/wpsilvaExperimentos/portarias/ifes-portarias-61933-indice-html/logo-ufes.png' width='100' height='100'><img src='http://rii.lcad.inf.ufes.br/wpsilvaExperimentos/portarias/ifes-portarias-61933-indice-html//logo-lcad.png'  width='100' height='100'></h1> <table width='70%'"
+rodape = '</table></body></html>'        
+
+inLinha="<tr><td>"
+midLinha="</td><td>"
+fimLinha="</td></tr>"
+
+
+
 def SaveColecao(colecao):
-    with open("labels-extraidos-48-portarias.csv",encoding='utf-8', mode="w+") as file:
-        writer = csv.writer(file, delimiter=";")
+    listaFeatures = []
+    listaDocumentos = []
+    with open("NER-50-portarias.html",encoding='utf-8', mode="w+") as file:
+        file.write("%s\n" % str(topo))
+        #writer = csv.writer(file, delimiter=" ")        
+        #writer.writerow(topo)
         for i in range(0, len(colecao)):
-            doc = colecao[i]
+            
+            doc = colecao[i]            
             filename = doc[0]
             paginas = doc[1]
-            feat3 = doc[2]            
-            linha = (filename, paginas, feat3)            
-            print(linha)
-            writer.writerow(linha)
+            feat3 = doc[2]
+            listaFeatures.append(feat3)                        
+            ln = ["<a href='http://rii.lcad.inf.ufes.br/wpsilvaExperimentos/portarias/ifes-portarias-61933-indice-html/",filename, ".txt' target='_blank'>" ,filename , '</a><br>']            
+            link = ''.join(ln)
+            linha = (inLinha, filename,midLinha, paginas,midLinha, feat3,midLinha, link, fimLinha)            
+            
+            file.write(''.join(linha))
+        file.write(rodape)
     file.close()
+    return listaFeatures, listaDocumentos
     
     
 
-SaveColecao(labels)
-
-import pandas as pd
+features, documentos = SaveColecao(labels)
 
 
-L = ['Thanks You', 'Its fine no problem', 'Are you sure']
+counts = dict()
+for i in features:
+  counts[i] = counts.get(i, 0) + 1
 
 
-'''
-#create new df 
-dfLabel = pd.DataFrame(labels)
-dfLabel.colnames(dfLabel)[0] <- "feat"
-dfLabel.colnames(dfLabel)[1] <- "label"
-print (dfLabel)
 
 
-from sklearn.preprocessing import LabelEncoder
 
-lb_make = LabelEncoder()
-dfLabel["make_code"] = lb_make.fit_transform(obj_df["make"])
-obj_df[["make", "make_code"]].head(0)
+with open('NER-Sumarizacao.csv', 'w') as f:
+    for key in counts.keys():
+        f.write("%s,%s\n"%(key,counts[key]))
 
-'''
 
-# one specific item's data
-#print('\nItem #2 data:')  
-#print(root[0][1].text)
 
-# all items data
-'''        
-print('\nAll item data:')  
-for elem in root:  
-    for subelem in elem:
-        print(subelem.text)
-'''
+
+
+
+
+
